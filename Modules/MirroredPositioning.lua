1de6c5b8f1f9cc48cfa05c33a6b38839f7a2769e
M1ch431 <46256998+M1ch431@users.noreply.github.com>
2019-09-08 14:22:26 -0400

    Fix #17

--- Modules/MirroredPositioning.lua
+++ Modules/MirroredPositioning.lua
@@ -3,68 +3,69 @@
 
 function MirroredPositioning()
 	if cfg.mirroredPositioning then
+		if not InCombatLockdown() then
+			local function SetMirroredPlayerPosition(a, b)
+				local _, _, _, offsetX, offsetY = b:GetPoint(1)
+				local mirroredOffsetX = GetScreenWidth() - offsetX - b:GetWidth()
 
-		local function SetMirroredPlayerPosition(a, b)
-			local _, _, _, offsetX, offsetY = b:GetPoint(1)
-			local mirroredOffsetX = GetScreenWidth() - offsetX - b:GetWidth()
+				a:ClearAllPoints()
+				a:SetPoint("TOPLEFT", nil, "TOPLEFT", mirroredOffsetX, offsetY)
+			end
 
-			a:ClearAllPoints()
-			a:SetPoint("TOPLEFT", nil, "TOPLEFT", mirroredOffsetX, offsetY)
-		end
+			local function SetMirroredTargetPosition(a, b)
+				local _, _, _, offsetX, offsetY = b:GetPoint(1)
+				local mirroredOffsetX = GetScreenWidth() - offsetX - a:GetWidth()
 
-		local function SetMirroredTargetPosition(a, b)
-			local _, _, _, offsetX, offsetY = b:GetPoint(1)
-			local mirroredOffsetX = GetScreenWidth() - offsetX - a:GetWidth()
+				a:ClearAllPoints()
+				a:SetPoint("TOPLEFT", nil, "TOPLEFT", mirroredOffsetX, offsetY)
+			end
 
-			a:ClearAllPoints()
-			a:SetPoint("TOPLEFT", nil, "TOPLEFT", mirroredOffsetX, offsetY)
-		end
+			local initialPositionPoint = {
+				[PlayerFrame] = "RIGHT",
+				[TargetFrame] = "LEFT"
+			}
 
-		local initialPositionPoint = {
-			[PlayerFrame] = "RIGHT",
-			[TargetFrame] = "LEFT"
-		}
+			local function SetInitialPosition(frame)
+				frame:SetUserPlaced(true)
+				frame:ClearAllPoints()
+				frame:SetPoint(initialPositionPoint[frame], UIParent, "CENTER")
+			end
 
-		local function SetInitialPosition(frame)
-			frame:SetUserPlaced(true)
-			frame:ClearAllPoints()
-			frame:SetPoint(initialPositionPoint[frame], UIParent, "CENTER")
-		end
+			hooksecurefunc("TargetFrame_ResetUserPlacedPosition", function(...)
+				SetInitialPosition(TargetFrame)
 
-		hooksecurefunc("TargetFrame_ResetUserPlacedPosition", function(...)
-			SetInitialPosition(TargetFrame)
+				if PlayerFrame:IsUserPlaced() then
+					SetInitialPosition(PlayerFrame)
+				end
+			end)
 
-			if PlayerFrame:IsUserPlaced() then
+			hooksecurefunc("PlayerFrame_ResetUserPlacedPosition", function(...)
+				SetInitialPosition(PlayerFrame)
+
+				if TargetFrame:IsUserPlaced() then
+					SetInitialPosition(TargetFrame)
+				end
+			end)
+
+			if not PlayerFrame:IsUserPlaced() then
 				SetInitialPosition(PlayerFrame)
 			end
-		end)
 
-		hooksecurefunc("PlayerFrame_ResetUserPlacedPosition", function(...)
-			SetInitialPosition(PlayerFrame)
-
-			if TargetFrame:IsUserPlaced() then
+			if not TargetFrame:IsUserPlaced() then
 				SetInitialPosition(TargetFrame)
 			end
-		end)
 
-		if not PlayerFrame:IsUserPlaced() then
-			SetInitialPosition(PlayerFrame)
-		end
-
-		if not TargetFrame:IsUserPlaced() then
-			SetInitialPosition(TargetFrame)
-		end
-
-		local function FocusedFrame_OnUpdate(self, elapsed)
-			if PlayerFrame:IsDragging() then
-				SetMirroredPlayerPosition(TargetFrame, PlayerFrame)
-			elseif TargetFrame:IsDragging() then
-				SetMirroredTargetPosition(PlayerFrame, TargetFrame)
+			local function FocusedFrame_OnUpdate(self, elapsed)
+				if PlayerFrame:IsDragging() then
+					SetMirroredPlayerPosition(TargetFrame, PlayerFrame)
+				elseif TargetFrame:IsDragging() then
+					SetMirroredTargetPosition(PlayerFrame, TargetFrame)
+				end
 			end
+
+			local FocusedFrame = CreateFrame("Frame")
+
+			FocusedFrame:SetScript("OnUpdate", FocusedFrame_OnUpdate)
 		end
-
-		local FocusedFrame = CreateFrame("Frame")
-
-		FocusedFrame:SetScript("OnUpdate", FocusedFrame_OnUpdate)
 	end
 end
