d1bbc75b029e1c4101a7e61c77ffb502edb38521
M1ch431 <46256998+M1ch431@users.noreply.github.com>
2019-09-09 09:42:28 -0400

    Code cleanup

--- Modules/Target.lua
+++ Modules/Target.lua
@@ -70,6 +70,76 @@ function TargetFrameStyling()
 	hooksecurefunc("TargetFrame_CheckClassification", function(self, forceNormalTexture)
 		local classification = UnitClassification(self.unit)
 
+		local function Styling()
+			if cfg.bigTargetHealthBar then
+				self.healthbar:SetHeight(27)
+				self.highLevelTexture:ClearAllPoints()
+				self.highLevelTexture:SetPoint("CENTER", self.manabar, "BOTTOMRIGHT", 53.5, -2.5)
+				self.manabar:SetHeight(12)
+				self.manabar:ClearAllPoints()
+				self.manabar:SetPoint("TOPLEFT", self, 7, -52)
+			else
+				self.healthbar:SetHeight(18)
+				self.highLevelTexture:ClearAllPoints()
+				self.highLevelTexture:SetPoint("CENTER", self.manabar, "BOTTOMRIGHT", 53.5, -4.5)
+				self.manabar:SetHeight(17)
+				self.manabar:ClearAllPoints()
+				self.manabar:SetPoint("TOPLEFT", self, 7, -45)
+			end
+
+			if cfg.wideTargetFrame and cfg.wideTargetFrameWidth >= 231 then
+				TargetFrameBackground:SetSize(cfg.wideTargetFrameWidth - 115, 38)
+				self.Background:SetSize(cfg.wideTargetFrameWidth - 115, 38)
+				self.healthbar:SetWidth(cfg.wideTargetFrameWidth - 115)
+				self.manabar:SetWidth(cfg.wideTargetFrameWidth - 115)
+			else
+				TargetFrameBackground:SetSize(117, 38)
+				self.Background:SetSize(117, 38)
+				self.healthbar:SetWidth(117)
+				self.manabar:SetWidth(117)
+			end
+
+			if isClassic() then
+				TargetFrameHealthBar.LeftText:ClearAllPoints()
+				TargetFrameHealthBar.LeftText:SetPoint("LEFT", self.healthbar, 1, 0)
+				TargetFrameHealthBar.RightText:ClearAllPoints()
+				TargetFrameHealthBar.RightText:SetPoint("RIGHT", self.healthbar, 0, 0)
+				TargetFrameHealthBarText:ClearAllPoints()
+				TargetFrameHealthBarText:SetPoint("CENTER", self.healthbar, 0, 0)
+				TargetFrameManaBar.LeftText:ClearAllPoints()
+				TargetFrameManaBar.LeftText:SetPoint("LEFT", self.manabar, 1, 0)
+				TargetFrameManaBar.RightText:ClearAllPoints()
+				TargetFrameManaBar.RightText:SetPoint("RIGHT", self.manabar, 0, 0)
+				TargetFrameManaBarText:ClearAllPoints()
+				TargetFrameManaBarText:SetPoint("CENTER", self.manabar, 0, 0)
+			else
+				self.healthbar.TextString:ClearAllPoints()
+				self.healthbar.TextString:SetPoint("CENTER", self.healthbar, 0, 0)
+				self.healthbar.LeftText:ClearAllPoints()
+				self.healthbar.LeftText:SetPoint("LEFT", self.healthbar, 1, 0)
+				self.healthbar.RightText:ClearAllPoints()
+				self.healthbar.RightText:SetPoint("RIGHT", self.healthbar, 0, 0)
+				self.manabar.LeftText:ClearAllPoints()
+				self.manabar.LeftText:SetPoint("LEFT", self.manabar, 1, 0)
+				self.manabar.RightText:ClearAllPoints()
+				self.manabar.RightText:SetPoint("RIGHT", self.manabar, 0, 0)
+				self.manabar.TextString:ClearAllPoints()
+				self.manabar.TextString:SetPoint("CENTER", self.manabar, 0, 0)
+			end
+
+			self.Background:ClearAllPoints()
+			self.Background:SetPoint("BOTTOMRIGHT", self, -108, 38)
+			self.healthbar:ClearAllPoints()
+			self.healthbar:SetPoint("TOPLEFT", self, 7, -24)
+			self.nameBackground:Hide()
+			self.nameBackground:SetAlpha(0)
+			self.deadText:ClearAllPoints()
+			self.deadText:SetPoint("CENTER", self.healthbar, 0, 0)
+			self.name:ClearAllPoints()
+			self.name:SetPoint("TOPLEFT", self, 8, -7.5)
+			self.name:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", -110, -19.5)
+		end
+
 		if forceNormalTexture then
 			if cfg.bigTargetHealthBar then
 				self.borderTexture:SetTexture("Interface\\AddOns\\EnhancedUnitFrames\\Media\\TargetFrame")
@@ -159,142 +229,12 @@ function TargetFrameStyling()
 				self.name:SetPoint("TOPLEFT", self, "TOPLEFT", 8, -24.5)
 				self.name:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", -110, -37.5)
 			else
-				if cfg.bigTargetHealthBar then
-					self.healthbar:SetHeight(27)
-					self.highLevelTexture:ClearAllPoints()
-					self.highLevelTexture:SetPoint("CENTER", self.manabar, "BOTTOMRIGHT", 53.5, -2.5)
-					self.manabar:SetHeight(12)
-					self.manabar:ClearAllPoints()
-					self.manabar:SetPoint("TOPLEFT", self, 7, -52)
-				else
-					self.healthbar:SetHeight(18)
-					self.highLevelTexture:ClearAllPoints()
-					self.highLevelTexture:SetPoint("CENTER", self.manabar, "BOTTOMRIGHT", 53.5, -4.5)
-					self.manabar:SetHeight(17)
-					self.manabar:ClearAllPoints()
-					self.manabar:SetPoint("TOPLEFT", self, 7, -45)
-				end
-
-				if cfg.wideTargetFrame and cfg.wideTargetFrameWidth >= 231 then
-					self.Background:SetSize(cfg.wideTargetFrameWidth - 115, 38)
-					self.healthbar:SetWidth(cfg.wideTargetFrameWidth - 115)
-					self.manabar:SetWidth(cfg.wideTargetFrameWidth - 115)
-				else
-					self.Background:SetSize(117, 38)
-					self.healthbar:SetWidth(117)
-					self.manabar:SetWidth(117)
-				end
-
-				if isClassic() then
-					TargetFrameHealthBar.LeftText:ClearAllPoints()
-					TargetFrameHealthBar.LeftText:SetPoint("LEFT", self.healthbar, 1, 0)
-					TargetFrameHealthBar.RightText:ClearAllPoints()
-					TargetFrameHealthBar.RightText:SetPoint("RIGHT", self.healthbar, 0, 0)
-					TargetFrameHealthBarText:ClearAllPoints()
-					TargetFrameHealthBarText:SetPoint("CENTER", self.healthbar, 0, 0)
-					TargetFrameManaBar.LeftText:ClearAllPoints()
-					TargetFrameManaBar.LeftText:SetPoint("LEFT", self.manabar, 1, 0)
-					TargetFrameManaBar.RightText:ClearAllPoints()
-					TargetFrameManaBar.RightText:SetPoint("RIGHT", self.manabar, 0, 0)
-					TargetFrameManaBarText:ClearAllPoints()
-					TargetFrameManaBarText:SetPoint("CENTER", self.manabar, 0, 0)
-				else
-					self.healthbar.TextString:ClearAllPoints()
-					self.healthbar.TextString:SetPoint("CENTER", self.healthbar, 0, 0)
-					self.healthbar.LeftText:ClearAllPoints()
-					self.healthbar.LeftText:SetPoint("LEFT", self.healthbar, 1, 0)
-					self.healthbar.RightText:ClearAllPoints()
-					self.healthbar.RightText:SetPoint("RIGHT", self.healthbar, 0, 0)
-					self.manabar.LeftText:ClearAllPoints()
-					self.manabar.LeftText:SetPoint("LEFT", self.manabar, 1, 0)
-					self.manabar.RightText:ClearAllPoints()
-					self.manabar.RightText:SetPoint("RIGHT", self.manabar, 0, 0)
-					self.manabar.TextString:ClearAllPoints()
-					self.manabar.TextString:SetPoint("CENTER", self.manabar, 0, 0)
-				end
-
-				self.Background:ClearAllPoints()
-				self.Background:SetPoint("BOTTOMRIGHT", self, -108, 38)
-				self.healthbar:ClearAllPoints()
-				self.healthbar:SetPoint("TOPLEFT", self, 7, -24)
-				self.nameBackground:Hide()
-				self.nameBackground:SetAlpha(0)
-				self.deadText:ClearAllPoints()
-				self.deadText:SetPoint("CENTER", self.healthbar, 0, 0)
-				self.name:ClearAllPoints()
-				self.name:SetPoint("TOPLEFT", self, 8, -7.5)
-				self.name:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", -110, -19.5)
+				Styling()
 			end
 		else
 			self.haveElite = true
 
-			if cfg.bigTargetHealthBar then
-				self.healthbar:SetHeight(27)
-				self.highLevelTexture:ClearAllPoints()
-				self.highLevelTexture:SetPoint("CENTER", self.manabar, "BOTTOMRIGHT", 53.5, -2.5)
-				self.manabar:SetHeight(12)
-				self.manabar:ClearAllPoints()
-				self.manabar:SetPoint("TOPLEFT", self, 7, -52)
-			else
-				self.healthbar:SetHeight(18)
-				self.highLevelTexture:ClearAllPoints()
-				self.highLevelTexture:SetPoint("CENTER", self.manabar, "BOTTOMRIGHT", 53.5, -4.5)
-				self.manabar:SetHeight(17)
-				self.manabar:ClearAllPoints()
-				self.manabar:SetPoint("TOPLEFT", self, 7, -45)
-			end
-
-			if cfg.wideTargetFrame and cfg.wideTargetFrameWidth >= 231 then
-				TargetFrameBackground:SetSize(cfg.wideTargetFrameWidth - 115, 38)
-				self.Background:SetSize(cfg.wideTargetFrameWidth - 115, 38)
-				self.healthbar:SetWidth(cfg.wideTargetFrameWidth - 115)
-				self.manabar:SetWidth(cfg.wideTargetFrameWidth - 115)
-			else
-				TargetFrameBackground:SetSize(117, 38)
-				self.Background:SetSize(117, 38)
-				self.healthbar:SetWidth(117)
-				self.manabar:SetWidth(117)
-			end
-
-			if isClassic() then
-				TargetFrameHealthBar.LeftText:ClearAllPoints()
-				TargetFrameHealthBar.LeftText:SetPoint("LEFT", self.healthbar, 1, 0)
-				TargetFrameHealthBar.RightText:ClearAllPoints()
-				TargetFrameHealthBar.RightText:SetPoint("RIGHT", self.healthbar, 0, 0)
-				TargetFrameHealthBarText:ClearAllPoints()
-				TargetFrameHealthBarText:SetPoint("CENTER", self.healthbar, 0, 0)
-				TargetFrameManaBar.LeftText:ClearAllPoints()
-				TargetFrameManaBar.LeftText:SetPoint("LEFT", self.manabar, 1, 0)
-				TargetFrameManaBar.RightText:ClearAllPoints()
-				TargetFrameManaBar.RightText:SetPoint("RIGHT", self.manabar, 0, 0)
-				TargetFrameManaBarText:ClearAllPoints()
-				TargetFrameManaBarText:SetPoint("CENTER", self.manabar, 0, 0)
-			else
-				self.healthbar.TextString:ClearAllPoints()
-				self.healthbar.TextString:SetPoint("CENTER", self.healthbar, 0, 0)
-				self.healthbar.LeftText:ClearAllPoints()
-				self.healthbar.LeftText:SetPoint("LEFT", self.healthbar, 1, 0)
-				self.healthbar.RightText:ClearAllPoints()
-				self.healthbar.RightText:SetPoint("RIGHT", self.healthbar, 0, 0)
-				self.manabar.LeftText:ClearAllPoints()
-				self.manabar.LeftText:SetPoint("LEFT", self.manabar, 1, 0)
-				self.manabar.RightText:ClearAllPoints()
-				self.manabar.RightText:SetPoint("RIGHT", self.manabar, 0, 0)
-				self.manabar.TextString:ClearAllPoints()
-				self.manabar.TextString:SetPoint("CENTER", self.manabar, 0, 0)
-			end
-
-			self.Background:ClearAllPoints()
-			self.Background:SetPoint("BOTTOMRIGHT", self, -108, 38)
-			self.healthbar:ClearAllPoints()
-			self.healthbar:SetPoint("TOPLEFT", self, 7, -24)
-			self.nameBackground:Hide()
-			self.nameBackground:SetAlpha(0)
-			self.deadText:ClearAllPoints()
-			self.deadText:SetPoint("CENTER", self.healthbar, 0, 0)
-			self.name:ClearAllPoints()
-			self.name:SetPoint("TOPLEFT", self, 8, -7.5)
-			self.name:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", -110, -19.5)
+			Styling()
 		end
 	end)
 
